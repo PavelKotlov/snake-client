91046da9594b0aacf665406b278fbd22c3a972e1
PavelKotlov <pavelkotlov@gmail.com>
2023-02-15 15:47:47 -0800

    Updated both input and play files to be able to pass the connection object from play to input inorder to be able to write to it the movements the snake needs to make during the game.

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -11,7 +14,18 @@ const handleUserInput = function (data) {
   if (data === "\u0003") {
     console.log("Successfully disconnected from game server");
     process.exit();
-  };
+  } else {
+    if (data == "w") {
+      connection.write("Move: up");
+    } else if (data == "s") {
+      connection.write("Move: down");
+    } else if (data == "a") {
+      connection.write("Move: left");
+    } else if (data == "d") {
+      connection.write("Move: right");
+    }
+
+  }
 };
 
 const input = {
