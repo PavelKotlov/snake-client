a1c07cf193fe052361cca8f2ee571390932bb83a
PavelKotlov <pavelkotlov@gmail.com>
2023-02-15 15:10:26 -0800

    Added key control to play.js to exit and terminate connection to game on ctrl+c

--- play.js
+++ play.js
@@ -1,4 +1,22 @@
 const connect = require('./client');
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.on("data", handleUserInput);
+  stdin.resume();
+  return stdin;
+};
+const inspect = require('util').inspect;
+const handleUserInput = function (data) {
+  console.log("The data is: ", inspect(data));
+  if (data === "\u0003") {
+    process.exit();
+    console.log("Successfully disconnected from game server");
+  };
+};
+
+setupInput();
 console.log("Connecting ...");
 connect();
